21e389e07923b02e861dd5468827e6709c81265f
agwebryanmuna <agwebryan1@gmail.com>
2025-05-17 23:07:36 +0100

    changed navbar background onscroll

--- main.js
+++ main.js
@@ -2,8 +2,9 @@
 const navbar = document.getElementById("navbar");
 const menuIcon = document.getElementById("menuIcon");
 const closeIcon = document.getElementById("closeIcon");
+const header = document.getElementById("mainHeader");
 
-// evennt listeners
+// event listeners
 menuIcon.addEventListener("click", () => {
   navbar.classList.replace("-right-[100%]", "right-0");
   menuIcon.classList.add("hidden");
@@ -14,4 +15,18 @@ closeIcon.addEventListener("click", () => {
   navbar.classList.replace("right-0", "-right-[100%]");
   closeIcon.classList.add("hidden");
   menuIcon.classList.remove("hidden");
+});
+
+window.addEventListener("scroll", () => {
+  const scrollHeight = window.pageYOffset;
+  const headerHeight = header.getBoundingClientRect().height;
+  if (scrollHeight > headerHeight) {
+    header.classList.remove("md:bg-transparent");
+    header.classList.remove("md:h-20");
+    header.classList.add("shadow-md");
+  } else {
+    header.classList.add("md:bg-transparent");
+    header.classList.add("md:h-20");
+    header.classList.remove("shadow-md");
+  }
 });
